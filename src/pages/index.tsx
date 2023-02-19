7d3d4841b6c82b24ef2002615a53721a2be7f495
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-02-19 20:56:39 +0900

    feat: Home 페이지 초기 화면 구현
    - TopBar 적용 및 첫 화면 구현(wave)
    - 페이지에 Aos 라이브러리 적용
    - 일부 css 파일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,43 @@
-import { Inter } from "@next/font/google";
+import Head from "next/head";
+import TopBar from "components/TopBar";
+import AOS from "aos";
+import styles from "@/styles/Home.module.css";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  });
+  return (
+    <>
+      <Head>
+        <title>WINK: Web IN Kookmin</title>
+        <meta
+          name="description"
+          content="국민대학교 소프트웨어융합대학 웹 학술 동아리 윙크😉"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <TopBar />
+      <div className={styles.wave}>
+        <p
+          data-aos="zoom-in"
+          data-aos-once="true"
+        >
+          <span>찡긋.</span> 우리 안의 일렁임을 불태울
+          <br />
+          가장 나다운 방법.
+        </p>
+        <p
+          data-aos="fade-up"
+          data-aos-delay="1000"
+        >지원하기 {`>`}</p>
+      </div>
+    </>
+  );
 }
