4c4545530e31750fe99da3b2e9fb291a7882bf22
sirldev <sirl0@naver.com>
2023-02-25 21:12:55 +0900

    refactor: Edit TopBar css to tailwind

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,15 +1,37 @@
 import Link from "next/link";
-import styles from "@/styles/TopBar.module.css";
 import Image from "next/image";
 import logo from "../public/logo.png";
 import { useEffect, useState } from "react";
 
-const TopBar = () => {
+function TopBar() {
   const [scrollY, setScrollY] = useState(0);
   const updateScroll = () => {
-    console.log(window.scrollY);
     setScrollY(window.scrollY);
   };
+
+  const navigations = [
+    {
+      title: "동아리 소개",
+      href: "/",
+      mobileHide: true,
+    },
+    {
+      title: "활동 소개",
+      href: "/",
+      mobileHide: true,
+    },
+    {
+      title: "부원 소개",
+      href: "/",
+      mobileHide: true,
+    },
+    {
+      title: "지원하기",
+      href: "/apply",
+      mobileHide: false,
+    },
+  ];
+
   useEffect(() => {
     const timer = setInterval(() => {
       window.addEventListener("scroll", updateScroll);
@@ -21,39 +43,39 @@ const TopBar = () => {
   }, []);
 
   return (
-    <div
-      className={`${styles.topbar}
-        ${scrollY !== 0 ? `${styles.topbar_under}` : ""}`}
+    <header
+      className={`fixed top-0 z-50 bg-white w-full h-14 flex items-center justify-center ${
+        scrollY !== 0 && " border-b"
+      }`}
     >
-      <Link href={"/"} replace>
-        <Image
-          className={styles.logo}
-          src={logo}
-          alt="Logo of WINK"
-          placeholder="blur"
-          width={89}
-          height={25}
-          priority
-        />
-      </Link>
-      <nav className={styles.center}>
-        <ul className={styles.item}>
-          <li>
-            <Link href={"/"}>동아리 소개</Link>
-          </li>
-          <li>
-            <Link href={"/"}>활동 소개</Link>
-          </li>
-          <li>
-            <Link href={"/"}>부원 소개</Link>
-          </li>
-          <li>
-            <Link href={"/apply"}>지원하기</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+      <div className="w-full max-w-6xl flex justify-between p-4 lg:p-6">
+        <Link href={"/"} replace>
+          <Image
+            className="object-contain h-6"
+            src={logo}
+            alt="Logo of WINK"
+            placeholder="blur"
+            height={24}
+            priority
+          />
+        </Link>
+        <nav className="flex justify-center items-center">
+          <ul className="flex justify-center items-center list-none gap-8">
+            {navigations.map((link) => (
+              <li
+                key={link.title}
+                className={`font-bold text-sm last:text-[#3a70ff] ${
+                  link.mobileHide && "hidden sm:block"
+                }`}
+              >
+                <Link href={link.href}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
-};
+}
 
 export default TopBar;
