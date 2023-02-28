a033e9f4089e0f67fe0aa86ecb60f992200a582a
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-02-28 14:04:24 +0900

    feat: Apply 페이지 레이아웃 변경 및 반응형 적용
    - Apply 페이지의 첫 화면을 화면 전체를 채우도록 변경
    - Apply 페이지에 사용되는 텍스트 반응형 적용 (로켓 이미지 보류)

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -3,33 +3,37 @@ import TopBar from "components/TopBar";
 import Footer from "components/Footer";
 import Image from "next/image";
 import rocket from "../../public/rocket.png";
+import arrow from "../../public/arrow.svg";
 
 const Apply: NextPage = () => {
   return (
     <>
       <TopBar />
       <div className="flex flex-col align-center">
-        <div
-          className="flex justify-center items-center w-full h-[50vw] bg-[#c8d7ff] bg-bottom bg-contain bg-no-repeat bg-[url('../../public/cloud.png')]"
-        >
+        <div className="flex flex-col justify-center items-center w-full h-screen bg-[#c8d7ff] bg-bottom bg-contain bg-no-repeat bg-[url('../../public/cloud.png')]">
           <Image
             className="z-0 absolute top-16 right-10 w-4/12"
             src={rocket}
             alt={"rocket iamge"}
             width={500}
           />
-          <div className="z-10">
-            <h1 className="text-[48px] font-extrabold">
-              <span className="text-[#3a70ff]">Web IN Koomin</span>
-              <br />
-              신입 부원 모집
+          <div className="z-10 px-2">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold">
+              <span className="text-[#3a70ff]">WINK</span> 신입 부원 모집
             </h1>
-            <h2 className="text-2xl mt-6">🚀 윙크에서 우주까지 함께 할 동료를 모집합니다!</h2>
+            <h2 className="text-lg md:text-xl lg:text-2xl mt-1 lg:mt-4">
+              🚀 윙크에서 우주까지 함께 할 동료를 모집합니다!
+            </h2>
           </div>
+          <Image
+            className="absolute bottom-1 lg:bottom-7 animate-bounce h-12 cursor-pointer"
+            src={arrow}
+            alt={"scroll button"}
+          />
         </div>
-        <div className="relative w-full h-[940px] pb-[56.25%]">
+        <div className="relative w-full my-10 h-[940px] pb-[56.25%]">
           <iframe
-            className="absolute top-0 left-0 w-[calc(100%-80px)] bg-[#f4f7ff] border border-[#e4e4e4] rounded-2xl mx-10" 
+            className="absolute top-0 left-0 w-[calc(100%-80px)] bg-[#f4f7ff] border border-[#e4e4e4] rounded-2xl mx-10"
             width="780"
             height="940"
             src=""
