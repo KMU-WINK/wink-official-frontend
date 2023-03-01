0ff150447c0d93e05d9001b98bff3e393322f9b5
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-03-02 01:48:34 +0900

    feat: 프로필 카드 이미지 GitHub 연결
    - 프로필 카드 이미지를 GitHub 프로필 이미지를 사용하도록 코드 수정
    - 외부 이미지 참조를 위하여 config 파일에 domain 주소 추가
    - 기본 프로필(profile.png) 사용 임시 보류

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["github.com"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
