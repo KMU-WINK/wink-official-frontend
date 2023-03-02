61135f3eaa1ec374ce0319c4225de34be94a4176
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-03-02 19:00:17 +0900

    chore: Apply 페이지 aos 추가

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -8,23 +8,26 @@ import cloud from "../../public/cloud.png";
 import useMoveScroll from "@/util/useMoveScroll";
 
 const Apply: NextPage = () => {
-  const {element, onMoveToElement} = useMoveScroll()
+  const { element, onMoveToElement } = useMoveScroll();
   return (
     <>
       <TopBar />
       <div className="flex flex-col align-center">
         <div className="flex flex-col justify-center items-center w-full h-screen bg-[#c8d7ff]">
           <Image
+            data-aos="fade-up-right"
+            data-aos-offset="800"
+            data-aos-once="true"
             className="z-0 absolute top-16 right-10 w-4/12"
             src={rocket}
             alt={"rocket iamge"}
             width={500}
           />
           <div className="z-10 px-2">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold">
+            <h1 data-aos="fade" data-aos-delay="800" className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold">
               <span className="text-[#3a70ff]">WINK</span> 신입 부원 모집
             </h1>
-            <h2 className="text-lg md:text-xl lg:text-2xl mt-1 lg:mt-4">
+            <h2 data-aos="fade" data-aos-delay="1200" data-aos-duration="1600" className="text-lg md:text-xl lg:text-2xl mt-1 lg:mt-4">
               🚀 윙크에서 우주까지 함께 할 동료를 모집합니다!
             </h2>
           </div>
@@ -41,7 +44,10 @@ const Apply: NextPage = () => {
             </div>
           </div>
         </div>
-        <div ref={element} className="relative w-full my-10 h-[940px] pb-[56.25%]">
+        <div
+          ref={element}
+          className="relative w-full my-10 h-[940px] pb-[56.25%]"
+        >
           <iframe
             className="absolute mt-[92px] top-0 left-0 w-[calc(100%-80px)] bg-[#f4f7ff] border border-[#e4e4e4] rounded-2xl mx-10"
             width="780"
