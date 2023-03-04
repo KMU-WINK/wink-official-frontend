2d4ce5ed6520d02ef4059c648c95c3877671b916
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-03-04 21:51:24 +0900

    refactor: Index 페이지 css -> tailwind
    - css module을 통한 style 속성들을 tailwind로 이전
    - 더미 내용값 제거 후 기입
    - 반응형 수치 일부 조정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,12 +73,22 @@ export default function Home() {
       </Head>
       <TopBar />
       <div className={styles.wave}>
-        <p data-aos="zoom-in" data-aos-once="true">
-          <span>찡긋.</span> 우리 안의 일렁임을 불태울
+        <h1
+          className="font-pretendard z-10 text-center font-bold text-3xl md:text-[53px] md:leading-[63.25px]"
+          data-aos="zoom-in"
+          data-aos-once="true"
+        >
+          <span className="font-medium text-2xl md:text-[47px] mb- md:mb-8">
+            나만의 서비스. 기획. 개발
+          </span>
           <br />
-          가장 나다운 방법.
-        </p>
-        <p data-aos="fade-up" data-aos-delay="1000">
+          우리 안의 새로운 물결 WINK
+        </h1>
+        <p
+          className="font-pretendard font-bold text-base md:text-xl text-[#3a70ff] z-10 "
+          data-aos="fade-up"
+          data-aos-delay="1000"
+        >
           <Link href={"/apply"}>지원하기 {`>`}</Link>
         </p>
       </div>
