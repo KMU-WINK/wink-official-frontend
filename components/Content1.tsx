c7102f643d2b9854ad479fe3561f5ddf9963f809
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-03-04 20:17:23 +0900

    refactor: props를 전달 받는 형태로 Content1 코드 리팩토링

--- components/Content1.tsx
+++ components/Content1.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-import image1 from "../public/intro_1.jpg";
-import image2 from "../public/intro_2.jpg";
+import Image, { StaticImageData } from "next/image";
 import "aos/dist/aos.css";
 
-const Content1 = () => {
+interface ContentProps {
+  text1: string;
+  text2: string;
+  imageSrc1: StaticImageData;
+  imageSrc2: StaticImageData;
+}
+
+const Content1 = ({ text1, text2, imageSrc1, imageSrc2 }: ContentProps) => {
   return (
     <div className="flex flex-col md:flex-row gap-12 md:gap-[80px]">
       <div className="flex flex-col gap-[58px]">
@@ -11,15 +16,17 @@ const Content1 = () => {
           className="font-pretendard text-4xl md:text-5xl font-medium"
           data-aos="fade-right"
         >
-          무시 못할,
-          <br />
-          자꾸만 눈이 가는
-          <br />큰 제목
+          {text1.split("\n").map((txt, index) => (
+            <p key={index}>
+              {txt}
+              <br />
+            </p>
+          ))}
         </h1>
         <Image
           data-aos="fade-up"
           className="rounded-3xl w-screen md:w-[400px]"
-          src={image1}
+          src={imageSrc1}
           alt={"Image 1"}
         />
       </div>
@@ -27,19 +34,20 @@ const Content1 = () => {
         <Image
           data-aos="fade-left"
           className="rounded-3xl w-screen md:w-[400px]"
-          src={image2}
+          src={imageSrc2}
           alt={"Image 2"}
         />
-        <p
+        <h3
           className="font-pretendard text-xl md:text-2xl font-medium"
           data-aos="fade-right"
         >
-          내용은 세 줄 정도면 좋겠어요.
-          <br />
-          솔직히 작은 글씨라 읽을까 싶기도 하지만,
-          <br />
-          아무말이나 적을 수는 없는 법이죠!
-        </p>
+          {text2.split("\n").map((txt, index) => (
+            <p key={index}>
+              {txt}
+              <br />
+            </p>
+          ))}
+        </h3>
       </div>
     </div>
   );
