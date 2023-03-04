ce956058fad9989a56bc85e93c2053194bcbf9f3
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-03-04 16:44:49 +0900

    refactor: Content2 컴포넌트 css -> tailwind

--- components/Content2.tsx
+++ components/Content2.tsx
@@ -1,4 +1,3 @@
-import styles from "@/styles/Content.module.css";
 import Image, { StaticImageData } from "next/image";
 import "aos/dist/aos.css";
 
@@ -10,17 +9,17 @@ interface ContentProps {
 
 const Content2 = ({ title, imageSrc, imageAlt }: ContentProps) => {
   return (
-    <div className={styles.content}>
-      <h1 data-aos="fade-right">
+    <div className="flex flex-col gap-[70px]">
+      <h1 className="font-pretendard text-black font-medium text-5xl" data-aos="fade-right">
         {title.split("\n").map((txt, index) => (
-          <p className={styles.reset} key={index}>
+          <p className="p-2" key={index}>
             {txt}
             <br />
           </p>
         ))}
       </h1>
       <Image
-        className={`${styles.image} ${styles.image_shadow}`}
+        className="rounded-3xl shadow-[0px 0px 100px rgba(255, 255, 255, 0.1)]"
         data-aos="zoom-in"
         data-aos-delay="200"
         src={imageSrc}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
-          rel="stylesheet"
-        />
-        <link
           rel="stylesheet"
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          type="text/css"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
       <body>
         <Main />
