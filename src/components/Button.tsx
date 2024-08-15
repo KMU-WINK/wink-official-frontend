8f46eab2eea04f502d708ce9f5137787b94d9942
Jinwook <stukids2765@gmail.com>
2024-08-15 13:47:12 +0900

    Feat: 로그인 버튼 함수 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 interface ButtonProps {
-    type: 'submit' | 'button' | 'reset';
-    label: string;
+    type: 'submit' | 'button' | 'reset',
+    label: string,
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ type, label }) => {
+const Button: React.FC<ButtonProps> = ({type, label, onClick}) => {
     return (
         <button
             type={type}
             className="w-full bg-[#9DB8FF] text-white py-2 rounded hover:bg-[#8aa7ff] focus:outline-none focus:ring-2 focus:ring-[#9DB8FF]"
+            onClick={onClick}
         >
             {label}
         </button>
