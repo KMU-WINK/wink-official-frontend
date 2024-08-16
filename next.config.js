0b1f0d07606d8b4086fe47a4ccb117a44cab845a
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-16 19:49:18 +0900

    feat(proxy): Backend Proxy 적용

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    if (process.env.NODE_ENV === "production") {
+      return [];
+    }
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ];
+  },
   images: {
     domains: ["github.com"],
   },
