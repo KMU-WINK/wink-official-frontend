c4d4c5e3f5419c62c94595f7980d1d484993570e
Jinwook <jinwook2765@naver.com>
2024-08-16 14:07:35 +0900

    Style: Button 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,14 @@ interface ButtonProps {
     type: 'submit' | 'button' | 'reset',
     label: string,
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({type, label, onClick}) => {
+const Button: React.FC<ButtonProps> = ({type, label, onClick, className}) => {
     return (
         <button
             type={type}
-            className="w-full bg-[#9DB8FF] text-white py-2 rounded hover:bg-[#8aa7ff] focus:outline-none focus:ring-2 focus:ring-[#9DB8FF]"
+            className={`text-white bg-[#9DB8FF] hover:bg-[#8aa7ff] rounded focus:outline-none focus:ring-2 focus:ring-[#9DB8FF] ${className}`}
             onClick={onClick}
         >
             {label}
