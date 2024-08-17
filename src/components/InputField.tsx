da537ad6d76cee8f5a5800064fdcf359f85f4357
Jinwook <jinwook2765@naver.com>
2024-08-17 22:24:34 +0900

    Feat: 회원가입 페이지 퍼블리싱

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,16 +4,22 @@ interface InputFieldProps {
     id: string;
     type: string;
     placeholder: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onFocus?: () => void;
 }
 
-const InputField: React.FC<InputFieldProps> = ({ id, type, placeholder }) => {
+const InputField: React.FC<InputFieldProps> = ({ id, type, placeholder, value, onChange, onFocus }) => {
     return (
             <input
                 type={type}
                 id={id}
                 name={id}
-                className="w-full px-3 py-4 border rounded focus:outline-none focus:ring-1 focus:ring-[#9DB8FF]"
+                className="w-full px-3 py-4 border border-[#C4C4C4] rounded focus:outline-none focus:ring-1 focus:ring-[#9DB8FF] placeholder-gray-600"
                 placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                onFocus={onFocus}
                 required
             />
     );
