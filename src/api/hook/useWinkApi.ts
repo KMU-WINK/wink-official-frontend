74793d7ffa294b4dd2ad5b9c25850314c3fc6050
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-21 18:07:00 +0900

    feature: Wink Official Backend API (#16)
    * feat(proxy): Backend Proxy 적용
    
    * feat(api): Backend API 구현
    
    * docs(api): Backend API Documentation
    
    * refactor(api): 쿠키 관리를 API에서 하도록
    
    * refactor(api): 사용하지 않는 import 삭제
    
    * style: prettier 적용
    
    * refactor: 어차피 prod환경에서도 host는 동일...
    
    * refactor: export user type
    
    * refactor: use react-cookie
    
    * feat: persistent user
    
    * feat: logout endpoint 추가
    
    * fix: 무한 루프 수정
    
    * fix: body를 optional로
    
    * feat: WinkApi Wrapper
    
    * docs: README.md 최신화
    
    * fix: comment 달기 귀찮
    
    * style: import order 및 폴더 구조 개선

---
+++ src/api/hook/useWinkApi.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+import { WinkApiContent, WinkApiResponse } from "@/api/request";
+
+export interface WinkApiHookResponse<T> {
+  fetching: boolean;
+  code: number;
+  error: boolean;
+  content: WinkApiContent<T>;
+}
+
+export function useWinkApi<T>(
+  result: Promise<WinkApiResponse<T>>,
+): WinkApiHookResponse<T> {
+  const [fetching, setFetching] = useState<boolean>(true);
+  const [code, setCode] = useState<number>(0);
+  const [error, setError] = useState<boolean>(false);
+  const [content, setContent] = useState<WinkApiContent<T>>(
+    new WinkApiContent<T>(""),
+  );
+
+  useEffect(() => {
+    result.then((response) => {
+      setCode(response.code);
+      setError(response.error);
+      setContent(response.content);
+      setFetching(false);
+    });
+  }, []);
+
+  return { fetching, code, error, content };
+}
---
+++ src/api/request/WinkApiRequest.ts
@@ -0,0 +1,182 @@
+import { Cookies } from "react-cookie";
+
+import { RefreshResponseDto, User } from "@/api/domain";
+
+import useUserStore from "@/store/useUserStore";
+
+interface WinkRawApiResponse<T> {
+  code: number;
+  error: boolean;
+  content: string | T;
+}
+
+export class WinkApiContent<T> {
+  public constructor(private readonly content: string | T) {}
+
+  public unwrap(): T {
+    return this.content as T;
+  }
+
+  public unwrapError(): string {
+    return this.content as string;
+  }
+}
+
+export class WinkApiResponse<T> {
+  private readonly _code: number;
+  private readonly _error: boolean;
+  private readonly _content: WinkApiContent<T>;
+
+  constructor(code: number, error: boolean, content: string | T) {
+    this._code = code;
+    this._error = error;
+    this._content = new WinkApiContent(content);
+  }
+
+  public get code(): number {
+    return this._code;
+  }
+
+  public get error(): boolean {
+    return this._error;
+  }
+
+  public get content(): WinkApiContent<T> {
+    return this._content;
+  }
+}
+
+export class WinkApiRequest {
+  private accessToken: string | null = null;
+  private refreshToken: string | null = null;
+
+  private readonly cookies = new Cookies();
+
+  public constructor() {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const accessToken: string | null = this.cookies.get("accessToken");
+    const refreshToken: string | null = this.cookies.get("refreshToken");
+
+    if (accessToken && refreshToken) {
+      this.setToken(accessToken, refreshToken);
+    }
+  }
+
+  private async request<T>(
+    url: string,
+    options: RequestInit,
+  ): Promise<WinkApiResponse<T>> {
+    const response = await fetch(`http://localhost:3000/api${url}`, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+    });
+
+    const apiResponse: WinkRawApiResponse<T> = await response.json();
+
+    if (
+      apiResponse.error &&
+      apiResponse.content === "접근 토큰이 만료되었습니다." &&
+      (await this.refresh())
+    ) {
+      return this.request(url, options);
+    }
+
+    return new WinkApiResponse(
+      apiResponse.code,
+      apiResponse.error,
+      apiResponse.content,
+    );
+  }
+
+  private async refresh(): Promise<boolean> {
+    const refreshResponse: WinkApiResponse<RefreshResponseDto> =
+      await this.post("/auth/refresh", { refreshToken: this.refreshToken });
+
+    if (!refreshResponse.error) {
+      return false;
+    }
+
+    const { accessToken, refreshToken } = refreshResponse.content.unwrap();
+
+    this.setToken(accessToken, refreshToken);
+
+    return true;
+  }
+
+  public async get<T>(url: string): Promise<WinkApiResponse<T>> {
+    return this.request(url, { method: "GET" });
+  }
+
+  public async post<T>(url: string, body?: any): Promise<WinkApiResponse<T>> {
+    return this.request(url, {
+      method: "POST",
+      body: body && JSON.stringify(body),
+    });
+  }
+
+  public async put<T>(url: string, body?: any): Promise<WinkApiResponse<T>> {
+    return this.request(url, {
+      method: "PUT",
+      body: body && JSON.stringify(body),
+    });
+  }
+
+  public async patch<T>(url: string, body?: any): Promise<WinkApiResponse<T>> {
+    return this.request(url, {
+      method: "PATCH",
+      body: body && JSON.stringify(body),
+    });
+  }
+
+  public async delete<T>(url: string): Promise<WinkApiResponse<T>> {
+    return this.request(url, { method: "DELETE" });
+  }
+
+  public setToken(accessToken: string | null, refreshToken: string | null) {
+    if (typeof window === "undefined") {
+      throw new Error("This method is only available in the browser.");
+    }
+
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+
+    if (accessToken === null || refreshToken === null) {
+      this.cookies.remove("accessToken");
+      this.cookies.remove("refreshToken");
+    } else {
+      this.cookies.set("accessToken", accessToken, {
+        path: "/",
+        expires: new Date(9999, 11, 31),
+      });
+      this.cookies.set("refreshToken", refreshToken, {
+        path: "/",
+        expires: new Date(9999, 11, 31),
+      });
+    }
+
+    this.updateUser();
+  }
+
+  private updateUser() {
+    if (this.accessToken === null) {
+      useUserStore.setState({ user: null });
+      return;
+    }
+
+    this.get("/auth/me").then((response) => {
+      if (response.error) {
+        this.cookies.remove("accessToken");
+        this.cookies.remove("refreshToken");
+        return;
+      }
+
+      useUserStore.setState({ user: response.content.unwrap() as User });
+    });
+  }
+}
