d799cfd610b7dbd5f93cc8145fb6f91d453f1679
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-07 12:00:31 +0900

    refactor(home): 스타일 인라인화

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,14 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        updown: {
-          from: { transform: 'translatey(0px)' },
-          to: { transform: 'translatey(-20px)' },
-        },
-        'updown-shadow': {
-          from: {
-            width: '184px',
-            '-webkit-filter': 'blur(6px)',
-          },
-          to: {
-            width: '240px',
-            '-webkit-filter': 'blur(10px)',
-          },
+        rotate: {
+          '0%': { transform: 'translate(-50%, 0) rotateZ(0deg)' },
+          '50%': { transform: 'translate(-50%, -2%) rotateZ(180deg)' },
+          '100%': { transform: 'translate(-50%, 0%) rotateZ(360deg)' },
         },
       },
       animation: {
-        updown: 'updown 1.4s infinite ease-in-out alternate',
-        'updown-shadow': 'updown-shadow 1.4s infinite ease-in-out alternate',
+        rotate: 'rotate 15s linear infinite',
       },
       fontFamily: {
         sans: ['pretendard', 'sans-serif'],
