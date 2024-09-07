6678a2599ee0376bc0286e89b3d3305325a29aad
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-07 14:32:08 +0900

    feat: form validation (#27)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,11 +7,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { Button, InputField } from '@/components';
+import { Button, InputFieldProps, InputField } from '@/components';
 
 import { WinkApi } from '@/api';
 
 import web_in_kookmin from '@/public/web_in_kookmin.svg';
+
+import * as yup from 'yup';
 
 type InputFieldId =
   | 'studentId'
@@ -21,50 +23,9 @@ type InputFieldId =
   | 'password'
   | 'confirmPassword';
 
-interface InputField {
-  id: InputFieldId;
-  type: string;
-  placeholder: string;
-  withButton?: boolean;
-}
-
-const inputFields: InputField[] = [
-  {
-    id: 'studentId',
-    type: 'text',
-    placeholder: '학번',
-  },
-  {
-    id: 'name',
-    type: 'text',
-    placeholder: '이름',
-  },
-  {
-    id: 'email',
-    type: 'email',
-    placeholder: '학교 이메일',
-    withButton: true,
-  },
-  {
-    id: 'verificationCode',
-    type: 'text',
-    placeholder: '인증번호',
-    withButton: true,
-  },
-  {
-    id: 'password',
-    type: 'password',
-    placeholder: '비밀번호',
-  },
-  {
-    id: 'confirmPassword',
-    type: 'password',
-    placeholder: '비밀번호 재확인',
-  },
-];
-
 export default function SignUp() {
   const router = useRouter();
+
   const [formData, setFormData] = useState({
     studentId: '',
     name: '',
@@ -73,25 +34,147 @@ export default function SignUp() {
     password: '',
     confirmPassword: '',
   });
+  const [sendCode, setSendCode] = useState<boolean>(false);
   const [verifyToken, setVerifyToken] = useState<string>('');
-  const [verificationComplete, setVerificationComplete] = useState(false);
-  const [showPasswordWarning, setShowPasswordWarning] = useState(false);
+
+  const inputFields: InputFieldProps<InputFieldId>[] = [
+    {
+      id: 'studentId',
+      type: 'text',
+      placeholder: '학번',
+      maxLength: 8,
+      validation: yup.string().required('학번을 입력해주세요.').length(8, '학번은 8자리입니다.'),
+    },
+    {
+      id: 'name',
+      type: 'text',
+      placeholder: '이름',
+      maxLength: 4,
+      validation: yup
+        .string()
+        .required('이름을 입력해주세요.')
+        .min(2, '이름은 2자리 이상입니다.')
+        .max(4, '이름은 4자리 이하입니다.')
+        .matches(/^[가-힣]+$/, '이름은 한글로 입력해주세요.'),
+    },
+    {
+      id: 'email',
+      type: 'email',
+      placeholder: '학교 이메일',
+      disabled: verifyToken !== '',
+      validation: yup
+        .string()
+        .required('이메일을 입력해주세요.')
+        .email('이메일 형식이 아닙니다.')
+        .matches(/^[a-zA-Z0-9._%+-]+@kookmin\.ac\.kr$/i, '국민대학교 이메일을 입력해주세요.'),
+      button: (
+        <Button
+          type="button"
+          label="인증번호 발송"
+          onClick={async () => {
+            await WinkApi.Auth.sendCode({
+              email: formData.email,
+            });
+
+            setSendCode(true);
+
+            toast.success('인증번호가 전송되었습니다.');
+          }}
+          className="text-[13px] px-1.5 py-3 w-28"
+        />
+      ),
+    },
+    {
+      id: 'verificationCode',
+      type: 'text',
+      placeholder: '인증번호',
+      maxLength: 6,
+      hidden: !sendCode || verifyToken !== '',
+      validation: yup
+        .string()
+        .required('인증번호를 입력해주세요.')
+        .length(6, '인증번호는 6자리입니다.'),
+      button: (
+        <Button
+          type="button"
+          label="확인"
+          onClick={async () => {
+            const { verifyToken } = await WinkApi.Auth.verifyCode({
+              email: formData.email,
+              code: formData.verificationCode,
+            });
+
+            setVerifyToken(verifyToken);
+
+            toast.success('인증이 완료되었습니다.');
+          }}
+          className="text-[13px] px-1.5 py-3 w-28"
+        />
+      ),
+    },
+    {
+      id: 'password',
+      type: 'password',
+      placeholder: '비밀번호',
+      validation: yup
+        .string()
+        .required('비밀번호를 입력해주세요.')
+        .min(8, '비밀번호는 8자리 이상입니다.')
+        .matches(
+          /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\d@$!%*?&#.~_-]{8,}$/,
+          '비밀번호는 영문, 숫자, 특수문자를 포함해야합니다.',
+        ),
+    },
+    {
+      id: 'confirmPassword',
+      type: 'password',
+      placeholder: '비밀번호 재확인',
+      validation: yup
+        .string()
+        .required('비밀번호를 입력해주세요.')
+        .test(
+          'passwords-match',
+          '비밀번호가 일치하지 않습니다.',
+          (value) => value === formData.password,
+        ),
+    },
+  ];
 
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
+
+    if (id === 'studentId' || id === 'verificationCode') {
+      if (isNaN(Number(value))) {
+        return;
+      }
+    }
+
     setFormData((prevState) => ({
       ...prevState,
       [id]: value,
     }));
-
-    if (id === 'verificationCode' && value.length > 0) {
-      setVerificationComplete(true);
-      setShowPasswordWarning(false);
-    }
   };
 
   const onClickSignUpButton = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+
+    try {
+      await Promise.all(
+        inputFields.map(async (field) => {
+          await field.validation?.validate(formData[field.id]);
+        }),
+      );
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        toast.error(error.message);
+        return;
+      }
+    }
+
+    if (!verifyToken) {
+      toast.error('이메일 인증을 완료해주세요.');
+      return;
+    }
 
     await WinkApi.Auth.register({
       name: formData.name,
@@ -103,33 +186,6 @@ export default function SignUp() {
     toast.success('회원 가입 요청이 완료되었습니다.');
 
     router.push('/');
-  };
-
-  const onSendVerificationCode = async () => {
-    await WinkApi.Auth.sendCode({
-      email: formData.email,
-    });
-
-    toast.success('인증번호가 전송되었습니다.');
-    setVerificationComplete(false);
-  };
-
-  const onConfirmVerifyCode = async () => {
-    const { verifyToken } = await WinkApi.Auth.verifyCode({
-      email: formData.email,
-      code: formData.verificationCode,
-    });
-
-    setVerifyToken(verifyToken);
-    setVerificationComplete(true);
-
-    toast.success('인증이 완료되었습니다.');
-  };
-
-  const onFocusPasswordInput = () => {
-    if (!verificationComplete) {
-      setShowPasswordWarning(true);
-    }
   };
 
   return (
@@ -147,43 +203,16 @@ export default function SignUp() {
 
         <div className="bg-white px-9 rounded-lg w-full max-w-md flex flex-col gap-[50px]">
           <div className="flex flex-col gap-[15px]">
-            {inputFields.map((field) => (
-              <div key={field.id} className="flex flex-col gap-[3px]">
-                <div className="flex items-center gap-[10px]">
-                  <InputField
-                    id={field.id}
-                    type={field.type}
-                    placeholder={field.placeholder}
-                    value={formData[field.id]}
-                    onChange={onChangeInput}
-                    onFocus={
-                      field.id === 'password' || field.id === 'confirmPassword'
-                        ? onFocusPasswordInput
-                        : undefined
-                    }
-                  />
-                  {field.withButton && field.id === 'email' && (
-                    <Button
-                      type="button"
-                      label="인증번호 발송"
-                      onClick={onSendVerificationCode}
-                      className="text-[13px] px-1.5 py-3 min-w-[100px]"
-                    />
-                  )}
-                  {field.withButton && field.id === 'verificationCode' && (
-                    <Button
-                      type="button"
-                      label="확인"
-                      onClick={onConfirmVerifyCode}
-                      className="text-[13px] px-1.5 py-3 min-w-[68px]"
-                    />
-                  )}
+            {inputFields
+              .filter((field) => !field.hidden)
+              .map((field) => (
+                <div key={field.id} className="flex flex-col gap-[3px]">
+                  <div className="flex items-center gap-[10px]">
+                    <InputField {...field} value={formData[field.id]} onChange={onChangeInput} />
+                    {field.button && field.button}
+                  </div>
                 </div>
-                {field.id === 'password' && showPasswordWarning && !verificationComplete && (
-                  <p className="text-[#666666] text-xs ml-3.5">인증을 완료해주세요.</p>
-                )}
-              </div>
-            ))}
+              ))}
           </div>
 
           <div className="flex flex-col items-center gap-[5px]">
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,33 +1,40 @@
-import React from 'react';
+'use client';
 
-interface InputFieldProps {
-  id: string;
-  type: string;
-  placeholder: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onFocus?: () => void;
+import React, { InputHTMLAttributes, ReactElement } from 'react';
+
+import * as yup from 'yup';
+
+export interface InputFieldProps<ID> extends Omit<InputHTMLAttributes<HTMLInputElement>, 'id'> {
+  id: ID;
+  validation?: yup.Schema<unknown>;
+  button?: ReactElement;
 }
 
-export const InputField: React.FC<InputFieldProps> = ({
-  id,
-  type,
-  placeholder,
-  value,
-  onChange,
-  onFocus,
-}) => {
+export const InputField: React.FC<InputFieldProps<string>> = (props) => {
+  const [error, setError] = React.useState<string | null>(null);
+
+  const proxyOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.validation
+      ?.validate(e.target.value)
+      .then(() => {
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+
+    props.onChange?.(e);
+  };
+
   return (
-    <input
-      type={type}
-      id={id}
-      name={id}
-      className="w-full px-3 py-4 border border-[#C4C4C4] rounded focus:outline-none focus:ring-1 focus:ring-[#9DB8FF] placeholder-gray-600"
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      onFocus={onFocus}
-      required
-    />
+    <div className="flex flex-col w-full">
+      <input
+        {...props}
+        className="w-full px-3 py-4 border border-[#C4C4C4] rounded focus:outline-none focus:ring-1 focus:ring-[#9DB8FF] placeholder-gray-600"
+        onChange={proxyOnChange}
+        required
+      />
+      {error && <p className="text-[12px] text-red-500 mt-1.5">{error}</p>}
+    </div>
   );
 };
