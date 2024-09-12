a0e9e09cb35338405fd18b74180d5c597c6c9dac
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-12 11:53:03 +0900

    refactor: tailwind arbitrary values 지양 (#32)
    * refactor(hook): useWinkApi를 api/hook에서 /hook으로
    
    * refactor(tailwind): Arbitrary values 지양

--- src/components/StudyCard.tsx
+++ src/components/StudyCard.tsx
@@ -26,19 +26,19 @@ export const StudyCard: React.FC<StudyCardProps> = ({
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex w-full justify-between items-center border border-[#ADADAD] px-5 py-2.5 rounded-[10px] hover:bg-gray-100"
+      className="flex w-full justify-between items-center border border-gray-400 px-5 py-2.5 rounded-lg hover:bg-gray-50"
     >
       {/* 스터디 제목 및 설명 */}
-      <div className="flex flex-col gap-[9px] my-[16.22px]">
-        <h2 className=" font-bold text-xl max-w-screen-sm truncate">{title}</h2>
-        <p className=" font-medium text-base text-[#868686] max-w-screen-sm truncate">
+      <div className="flex flex-col gap-2.5 my-4">
+        <h2 className="font-bold text-xl max-w-screen-sm truncate">{title}</h2>
+        <p className="font-medium text-base text-slate-500 max-w-screen-sm truncate">
           {description}
         </p>
-        <p className=" font-medium text-base max-w-screen-sm truncate">{link}</p>
+        <p className="font-medium text-base max-w-screen-sm truncate">{link}</p>
       </div>
 
       {/* 이미지 영역 */}
-      <div className="flex items-center flex-shrink-0 w-[139px] h-[112px]">
+      <div className="flex items-center flex-shrink-0 w-36 h-28">
         <Image src={image || logo} alt={title} width={139} height={112} />
       </div>
     </a>
