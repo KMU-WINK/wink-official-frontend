79e54f9ca49da466e85d24c04d7987753b9c9d0d
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-15 12:37:37 +0900

    refactor(api): File Upload 구현 (#34)

--- src/api/domain/index.ts
+++ src/api/domain/index.ts
@@ -2,6 +2,7 @@ export * from './Auth';
 export * from './Member';
 export * from './Activity';
 
+export * from './activity/Common';
 export * from './activity/Project';
 export * from './activity/Study';
 export * from './activity/Social';
