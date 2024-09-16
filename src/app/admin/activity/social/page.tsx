22a0e6ccd994494cd38649af94fa35d41b3cbb20
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-17 00:21:52 +0900

    feat: Admin Social Page 추가

--- src/app/admin/activity/social/page.tsx
+++ src/app/admin/activity/social/page.tsx
@@ -1,52 +1,74 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaEdit } from 'react-icons/fa';
 import { FaTrashCan } from 'react-icons/fa6';
+import { toast } from 'react-toastify';
 
-import Link from 'next/link';
+import { AdminIconButton, AdminSearchBar, AdminTablePaging, AdminTitle, Modal } from '@/component';
 
-import { AdminIconButton, AdminSearchBar, AdminTablePaging, AdminTitle } from '@/component';
-
-import { StudyType, WinkApi } from '@/api';
+import { SocialType, WinkApi } from '@/api';
 
 import { formatDate } from '@/util';
 
 const AdminActivitySocialPage = () => {
-  const [studies, setStudies] = useState<StudyType[]>([]);
+  const [socials, setSocials] = useState<SocialType[]>([]);
 
   const [page, setPage] = useState<number>(1);
   const [maxPage, setMaxPage] = useState<number>(1);
 
   const [query, setQuery] = useState<string>('');
 
-  useEffect(() => {
-    async function fetchingStudies() {
-      const { studies } = await WinkApi.Activity.Study.getStudies({ page });
-      setStudies(studies);
-    }
+  const [deleteModal, setDeleteModal] = useState<SocialType | null>(null);
 
+  const handleDelete = async () => {
+    if (!deleteModal) return;
+
+    await WinkApi.Activity.SocialAdmin.deleteSocial({ socialId: deleteModal._id });
+    await fetchSocials();
+
+    toast.warn(`"${deleteModal.title}" 친목 활동이 삭제되었습니다.`);
+
+    setDeleteModal(null);
+  };
+
+  useEffect(() => {
     async function fetchMaxPage() {
-      const { page } = await WinkApi.Activity.Study.getStudiesPage();
+      const { page } = await WinkApi.Activity.Social.getSocialsPage();
       setMaxPage(page);
     }
 
     (async () => {
-      await fetchingStudies();
       await fetchMaxPage();
     })();
-  }, [page]);
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      await fetchSocials();
+    })();
+  }, [page, query]);
+
+  async function fetchSocials() {
+    if (query) {
+      const { socials } = await WinkApi.Activity.Social.searchSocials({ query });
+      setSocials(socials);
+    } else {
+      const { socials } = await WinkApi.Activity.Social.getSocials({ page });
+      setSocials(socials);
+    }
+  }
 
   return (
     <div className="container mx-auto mt-4">
-      <AdminTitle title="Activity" subtitle="스터디" />
+      <AdminTitle title="Activity" subtitle="친목 활동" />
 
       <div className="flex justify-end mb-4 space-x-4">
         <AdminSearchBar value={query} placeholder="제목을 검색해주세요." onChange={setQuery} />
 
         <AdminIconButton
           icon={<FaEdit />}
-          text="스터디 추가"
+          text="친목 활동 추가"
           className="bg-wink-500 hover:bg-wink-600 border-0 text-white"
           onClick={() => {}}
         />
@@ -67,20 +89,42 @@ const AdminActivitySocialPage = () => {
         </div>
       </div>
 
-      {studies.map((study) => (
-        <div key={study._id} className="grid grid-cols-12 gap-2 border-b border-gray-200">
-          <Link href={study.link} className="py-4 px-4 col-span-7 text-sm truncate">
-            {study.title}
-          </Link>
-          <div className="py-4 px-4 col-span-3 text-sm">{formatDate(study.uploadedAt)}</div>
+      {socials.map((social) => (
+        <div key={social._id} className="grid grid-cols-12 gap-2 border-b border-gray-200">
+          <div className="py-4 px-4 col-span-7 text-sm truncate">{social.title}</div>
+          <div className="py-4 px-4 col-span-3 text-sm">{formatDate(social.createdAt)}</div>
           <div className="col-span-1 flex items-center justify-center space-x-8">
             <FaEdit size={18} className="cursor-pointer" onClick={() => {}} />
           </div>
           <div className="col-span-1 flex items-center justify-center space-x-8">
-            <FaTrashCan size={18} className="cursor-pointer" onClick={() => {}} />
+            <FaTrashCan
+              size={18}
+              className="cursor-pointer"
+              onClick={() => setDeleteModal(social)}
+            />
           </div>
         </div>
       ))}
+
+      <Modal isOpen={!!deleteModal} onClose={() => setDeleteModal(null)}>
+        <h2 className="text-xl font-bold mb-4">정말로 삭제하시겠습니까?</h2>
+        <p>{deleteModal?.title}</p>
+        <div className="flex justify-end mt-4">
+          <button
+            className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-md"
+            onClick={handleDelete}
+          >
+            삭제
+          </button>
+
+          <button
+            className="px-4 py-2 bg-gray-300 hover:bg-gray-400 text-gray-600 rounded-md ml-2"
+            onClick={() => setDeleteModal(null)}
+          >
+            취소
+          </button>
+        </div>
+      </Modal>
 
       {!query && <AdminTablePaging page={page} setPage={setPage} maxPage={maxPage} />}
     </div>
