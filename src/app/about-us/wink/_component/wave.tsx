3c44eeb2317cd1bf854540c37b49d3e58fda1e4c
Son Daehyeon <sondaehyeon01@gmail.com>
2025-02-11 16:05:01 +0900

    refactor(intro): 소개 페이지의 사진 교체

--- src/app/about-us/wink/_component/wave.tsx
+++ src/app/about-us/wink/_component/wave.tsx
@@ -14,7 +14,7 @@ export default function Wave({ children, className }: WaveProps) {
     <div className="flex flex-col items-center w-full">
       <div className="relative w-full h-[150px] sm:h-[250px] bg-wink-100 overflow-hidden">
         <motion.div
-          className="absolute -left-[100vw] bottom-[15vh] w-[300vw] h-[300vw] bg-white rounded-[47%]"
+          className="absolute -left-[50vw] sm:-left-[100vw] bottom-[10vh] sm:bottom-[15vh] w-[200vw] h-[200vw] sm:w-[300vw] sm:h-[300vw] bg-white rounded-[47%]"
           animate={{
             rotate: 360,
           }}
@@ -25,7 +25,7 @@ export default function Wave({ children, className }: WaveProps) {
         />
 
         <motion.div
-          className="absolute -left-[100vw] bottom-[15vh] w-[300vw] h-[300vw] bg-white/50 rounded-[45%]"
+          className="absolute -left-[50vw] sm:-left-[100vw] bottom-[10vh] sm:bottom-[15vh] w-[200vw] h-[200vw] sm:w-[300vw] sm:h-[300vw] bg-white/50 rounded-[45%]"
           animate={{
             rotate: 360,
           }}
@@ -38,7 +38,7 @@ export default function Wave({ children, className }: WaveProps) {
 
       <div
         className={cn(
-          'flex items-center justify-center w-full bg-wink-100 pb-12 sm:pb-18',
+          'flex items-center justify-center w-full bg-wink-100 py-12 sm:py-24',
           className,
         )}
       >
