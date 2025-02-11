1d7ce20e35f72129a3fa87293026ed0541bc7bf2
Son Daehyeon <sondaehyeon01@gmail.com>
2025-02-11 16:05:01 +0900

    refactor(intro): 소개 페이지의 사진 교체

--- src/app/about-us/wink/_constant/activity.ts
+++ src/app/about-us/wink/_constant/activity.ts
@@ -1,9 +1,9 @@
 import { StaticImageData } from 'next/image';
 
 import LinkImage from '@/public/about-us/wink/activity/link.avif';
-import WinkathonImage from '@/public/about-us/wink/activity/link.avif';
 import PreCourseImage from '@/public/about-us/wink/activity/precourse.avif';
 import WiminarImage from '@/public/about-us/wink/activity/wiminar.avif';
+import WinkathonImage from '@/public/about-us/wink/activity/winkathon.avif';
 
 interface Activity {
   tag: string;
