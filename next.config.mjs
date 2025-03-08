91a1f7a68ca640e9909a46e3f0e8e57ed71c94b4
Son Daehyeon <sondaehyeon01@gmail.com>
2025-03-08 19:08:59 +0900

    refactor(domain): 도메인 수정

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/api/:path*`,
+        destination: `${process.env.API_URL}/:path*`,
       },
     ];
   },
